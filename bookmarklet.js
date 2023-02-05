76f404389cc1c9f5663141668404f6affb3a23ae
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-02-05 10:40:00 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
+javascript:
 var html = "<iframe id='frame' src='https://redstone-nw.netlify.app/clockwork-app/index.html' width='100%' frameborder='0' style='height:100%; top:0; left:0; bottom:0; right:0; margin:0; width:100%; border:none; padding:0; overflow:hidden; z-index:999999;'> Your browser doesn't support iframes 🤡 </iframe>";
 var myWindow = window.open("", Math.random.toString(), "width=400,height=400");
 myWindow.document.body.style = "background-color: black; color:#888; margin: 0;";
@@ -6,5 +7,6 @@ myWindow.document.title = document.title;
 
 var link = document.createElement('link');
 link.rel = 'icon';
-link.href = document.querySelector("link[rel~='icon']").href;
 myWindow.document.head.appendChild(link);
+link.href = document.querySelector("link[rel~='icon']").href; //document.querySelector("link[rel~='icon']").href
+document.location.reload();
