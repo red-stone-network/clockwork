d6abb9576f3e6d9edf706bb27cfb06abc88f40c5
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-02-05 14:42:13 -0500

    Update app-template.js

--- app-template.js
+++ app-template.js
@@ -1,26 +1,32 @@
-////CONFIG////
+function start() {
 
-var link = "https://example.com/index.html"; // link to app webpage
-var id = "ExampleIFrame"; // a UNIQUE id to identify the frame for your app
-var title = "Example App"; // title shown in navbar
-var uninstallId = "https://sub64.netlify.app/clockwork-beta/protohub.js"; // set this to the url location of your script
+  ////CONFIG////
 
-////SCRIPT////
+  var link = "https://example.com/index.html"; // link to app webpage
+  var id = "ExampleIFrame"; // a UNIQUE id to identify the frame for your app
+  var title = "Example App"; // title shown in navbar
+  var uninstallId = "insert url location here"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.uninstallId = uninstallId;
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleLink.uninstallId, ExampleLink.className);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
 
-var ExampleLink = document.createElement("a");
-var ExampleIFrame = document.createElement("iframe");
-
-ExampleIFrame.style = "display: none;";
-ExampleIFrame.className = "app";
-ExampleIFrame.id = id; 
-ExampleIFrame.src = "about:blank";
-
-ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
-ExampleLink.innerHTML = title; 
-ExampleLink.uninstallId = uninstallId;
-ExampleLink.addEventListener('dblclick', (e) => {
-  uninstallApp(ExampleLink.uninstallId, [ExampleLink,ExampleIFrame]);
-});
-
-document.getElementById("navbar").appendChild(ExampleLink);
-document.getElementById("main").appendChild(ExampleIFrame);
+start();
