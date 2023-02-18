b0459830146da5122d53e51df0899aefdc6235e5
LukasExists <91223726+lukasexists@users.noreply.github.com>
2023-02-18 18:24:09 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
 javascript:
 var s = document.createElement('script');s.type='text/javascript';
 document.body.appendChild(s);
-s.src='//raw.githubusercontent.com/red-stone-network/clockwork/main/script.js';
+s.src='//redstone-nw.netlify.app/assets/clockwork-bookmarklet.js';
