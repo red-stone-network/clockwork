0bd04c4a863d9a12a093821223b26d15bba939f9
Lukas <toasterpanic@gmail.com>
2023-05-18 00:32:29 +0000

    beta13, polish Clockwork for release

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,38 @@
 /* Used across the Clockwork main pages */
 
 document.querySelector(".navbar").innerHTML = `<a href="https://discord.gg/Sb8NzVbqX8">Discord</a> | 
+<a href="/converter/">Convert v1 apps to v2</a> |
 © 2023 <a href="https://redstone-network.vercel.app/">Redstone Network</a>`
+
+
+// for /converter/
+function generateV2Code() {
+  var v1c = document.querySelector("textarea").value;
+
+  var appName = v1c.match(/title {0,1}= {0,1}"([^"]+)"/);
+  if (appName) appName = appName[1];
+
+  var appLink = v1c.match(/link {0,1}= {0,1}"([^"]+)"/);
+  if (appLink) appLink = appLink[1];
+
+  if (!appName || !appLink) {
+    alert("Invalid app!");
+    throw "InvalidApp";
+  }
+
+  var v2c = `{
+  "name": "${appName}",
+  "desc": "Converted from v1 to v2",
+  "url": "${appLink}",
+  "encodedUrl": "no",
+  "icon": "https://redstone-nw.netlify.app/assets/icon.png",
+  "author": "Type your name here",
+  "permissions": []
+}`;
+
+  document.querySelector("textarea").value = v2c;
+  document.querySelector("#instruction").innerHTML = "Now, you can copy the contents of the textarea into a .JSON file and save it somewhere. Make sure the service you use serves the correct Content-Type headers for .JSON files, such as Github Pages or Vercel.";
+}
 
 // for /get-started/
 if (document.location.pathname.startsWith("/get-started")) {
