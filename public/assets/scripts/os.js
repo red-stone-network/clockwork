2aafd62061bfe1555da6c4c23b32dc69c1949e72
Lukas <toasterpanic@gmail.com>
2023-07-12 21:31:47 +0000

    Update file os.js

--- public/assets/scripts/os.js
+++ public/assets/scripts/os.js
@@ -92,8 +92,7 @@ function loadSettingsScreen(screen) {
       textbox.class = "text";
       textbox.dataset.linked = settingsMenu[screen].screenContents[i].linkedSetting;
       textbox.onchange = (e) => {
-        alert(e.target);
-        eval(this.dataset.linked + " = " + this.value); 
+        eval(e.target.dataset.linked + " = " + this.value); 
         localStorage.setItem('settings', JSON.stringify(settings));
       }
 
