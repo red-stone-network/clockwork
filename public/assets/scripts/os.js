754182755ee3322f3ad9cd11d26af2bbd796fa37
Lukas <toasterpanic@gmail.com>
2023-07-14 00:01:33 +0000

    Update 5 files
    - /public/assets/scripts/os.js
    - /public/assets/images/wallpaper-previews/mountains.png
    - /public/assets/images/wallpaper-previews/grand-canyon.png
    - /public/assets/images/wallpaper-previews/sunset.png
    - /public/assets/images/wallpaper-previews/default.png

--- public/assets/scripts/os.js
+++ public/assets/scripts/os.js
@@ -146,27 +146,32 @@ const settingsMenu = [
           var defaultWallpapers = [
             {
               title: "Default",
-              url: "/assets/images/wallpapers/default.png"
+              url: "/assets/images/wallpapers/default.png",
+              preview: "/assets/images/wallpaper-previews/default.png"
             },
             {
               title: "Mountains",
-              url: "/assets/images/wallpapers/mountains.png"
+              url: "/assets/images/wallpapers/mountains.png",
+              preview: "/assets/images/wallpaper-previews/mountains.png"
             },
             {
               title: "Sunset by Quino Al",
-              url: "/assets/images/wallpapers/sunset.png"
+              url: "/assets/images/wallpapers/sunset.png",
+              preview: "/assets/images/wallpaper-previews/sunset.png"
             },
             {
               title: "Grand Canyon by Bernard Spragg",
-              url: "/assets/images/wallpapers/grand-canyon.jpg"
+              url: "/assets/images/wallpapers/grand-canyon.jpg",
+              preview: "/assets/images/wallpaper-previews/grand-canyon.png"
             }
           ]
           for (let i=0; i<defaultWallpapers.length;) {
             var paper = document.createElement("DIV");
             paper.innerText = defaultWallpapers[i].title;
-            paper.style.backgroundImage = `url(${defaultWallpapers[i].url})`;
+            paper.style.backgroundImage = `url(${defaultWallpapers[i].preview})`;
+            paper.dataset.url = defaultWallpapers[i].url;
             paper.onclick = function(e) {
-              settings.wallpaper = e.target.style.backgroundImage.slice(4, -1);
+              settings.wallpaper = e.target.dataset.url;
               localStorage.setItem("settings", JSON.stringify(settings));
             }
             div.appendChild(paper);
